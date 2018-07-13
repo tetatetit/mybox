2afc7b85ab7480d2d9bcb94a6df810b9c6031f58
Serhiy Berezin <berezins@live.com>
2018-07-13 21:22:20 +0300

    Update services.ps1

--- services.ps1
+++ services.ps1
@@ -1,14 +1,32 @@
 $servicesPath = "HKLM:\System\CurrentControlSet\Services"
-$startTypes = @{
-  Automatic = 2
-  Manual = 3
-  Disabled = 4
+
+function set-startType($serviceName, $startType) {
+  $servicePath = $servicesPath\$serviceName
+  if(-not ($defaultStartType = get-itemProperty -Path $servicePath -Name StartDefault)) {
+    Set-ItemProperty -Path $servicePath -Name StartDefault -Type DWord -Value $defaultStartType -Force
+  }
+  Set-ItemProperty -Path $servicePath -Name Start -Type DWord -Value $startType -Force
 }
 
-function Set-ServiceStartType($serviceName, $startType) {
-  $service = get-service -name $serviceName | select StartType,Name,DisplayName
-  if($service) {
-    export-cvs -inputObject $service -append C:\mybox-services.csv
-    Set-ItemProperty -Path "$servicesPath\$($svc.Name)" -Name Start -Type DWord -Value $startTypes[$startType] -Force
+function restore-startType($serviceName) {
+  $servicePath = $servicesPath\$serviceName
+  if($defaultStartType = get-itemProperty -Path $servicePath -Name StartDefault) {
+    set-ItemProperty -Path $servicePath -Name Start -Type DWord -Value $defaultStartType -Force
+    remove-ItemProperty -Path $servicePath -Name StartDefault
   }
 }
+
+function d($serviceName) {
+  set-startType $serviceName 4
+}
+
+function m($serviceName) {
+  set-startType $serviceName 3
+}
+
+function r($serviceName) {
+  restore-startType $serviceName
+}
+
+
+d wscsvc
