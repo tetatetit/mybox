c51376abb3bbeaecd21a2912b4a544a5ff0068a1
Serhiy Berezin <berezins@live.com>
2018-07-13 21:36:06 +0300

    Update services.ps1

--- services.ps1
+++ services.ps1
@@ -2,31 +2,32 @@ $servicesPath = "HKLM:\System\CurrentControlSet\Services"
 
 function set-startType($serviceName, $startType) {
   $servicePath = "$servicesPath\$serviceName"
-  if(-not ($defaultStartType = get-itemProperty -Path $servicePath -Name StartDefault -ErrorAction SilentlyContinue)) {
-    Set-ItemProperty -Path $servicePath -Name StartDefault -Type DWord -Value $defaultStartType -Force
+  if(-not get-itemProperty -Path $servicePath -Name StartDefault -ErrorAction SilentlyContinue) {
+    set-ItemProperty -Force -Path $servicePath -Name StartDefault -Type DWord \
+      -Value get-itemProperty -Path $servicePath -Name Start
   }
-  Set-ItemProperty -Path $servicePath -Name Start -Type DWord -Value $startType -Force
+  set-ItemProperty -Path $servicePath -Name Start -Type DWord -Value $startType -Force
 }
 
 function restore-startType($serviceName) {
   $servicePath = "$servicesPath\$serviceName"
-  if($defaultStartType = get-itemProperty -Path $servicePath -Name StartDefault) {
+  if($defaultStartType = get-itemProperty -Path $servicePath -Name StartDefault -ErrorAction SilentlyContinue) {
     set-ItemProperty -Path $servicePath -Name Start -Type DWord -Value $defaultStartType -Force
     remove-ItemProperty -Path $servicePath -Name StartDefault
   }
 }
 
-function d($serviceName) {
+function ds($serviceName) {
   set-startType $serviceName 4
 }
 
-function m($serviceName) {
+function ms($serviceName) {
   set-startType $serviceName 3
 }
 
-function r($serviceName) {
+function rs($serviceName) {
   restore-startType $serviceName
 }
 
 
-d wscsvc
+ds wscsvc
